0fb170f17565e07395859e572cfd48ed1ed54d63
Daniel Flores <dnielfs@gmail.com>
2013-07-03 17:08:44 -0500

    url for static

--- urls.py
+++ urls.py
@@ -7,3 +7,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('apps.api.urls')),
 )
+
+from django.conf import settings
+
+urlpatterns += patterns('',
+   (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
